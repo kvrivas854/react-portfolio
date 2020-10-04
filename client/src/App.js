6ab9a843b7a22697e8a50b9d359ae8964a5dd6e7
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-03 22:42:25 -0400

    updating server files

--- client/src/App.js
+++ client/src/App.js
@@ -11,27 +11,7 @@ import Experience from "./components/Experience/index";
 
 
 class App extends Component {
-  state = {
-      data: null
-    };
-  
-    componentDidMount() {
-        // Call our fetch function below once the component mounts
-      this.callBackendAPI()
-        .then(res => this.setState({ data: res.express }))
-        .catch(err => console.log(err));
-    }
-      // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    callBackendAPI = async () => {
-      const response = await fetch('/express_backend');
-      const body = await response.json();
-  
-      if (response.status !== 200) {
-        throw Error(body.message) 
-      }
-      return body;
-    };
-    render () {
+render() {
   return (
     <div>
     <Router>
@@ -47,7 +27,6 @@ class App extends Component {
     </div>
     
   );
-    }
 }
-
+    }
 export default App;
